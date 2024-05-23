a66f1c491a1c8c012d9d66bb5a64c77ed1e7e6be
Abdelilah El-Guennouni <142789585+elguennouni-dev@users.noreply.github.com>
2024-05-23 10:38:09 +0100

    c

--- index.php
+++ index.php
@@ -38,11 +38,46 @@ require 'database/config.php';
 
 
 
+<header id="header" class="header fixed-top">
+    <div class="topbar d-flex align-items-center">
+        <div class="container d-flex justify-content-center justify-content-md-between">
+            <div class="contact-info d-flex align-items-center">
+                <i class="bi bi-envelope d-flex align-items-center">
+                    <a href="mailto:brilliancemuseum@gmail.com">brilliancemuseum@gmail.com</a>
+                </i>
+                <i class="bi bi-phone d-flex align-items-center ms-4">
+                    <span>+1 5589 55488 55</span>
+                </i>
+            </div>
+            <div class="social-links d-none d-md-flex align-items-center">
+                <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+            </div>
+        </div>
+    </div><!-- End Top Bar -->
 
-<?php
-$page = 'index';
-include 'includes/header.php';
-?>
+    <div class="branding d-flex align-items-center">
+        <div class="container position-relative d-flex align-items-center justify-content-between">
+            <a href="<?php echo $home ?>" class="logo d-flex align-items-center">
+                <!-- Uncomment the line below if you also wish to use an image logo -->
+                <!-- <img src="assets/img/logo.png" alt=""> -->
+                <h1 class="sitename">Brilliance</h1>
+            </a>
+            <nav id="navmenu" class="navmenu">
+                <ul>
+                    <li><a href="#hero" class="">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="client/action/login.php">Login/Register</a></li>
+                </ul>
+                <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+            </nav>
+        </div>
+    </div>
+</header>
+
 
 
 
@@ -55,7 +90,7 @@ include 'includes/header.php';
             <div class="col-lg-8">
                <h2 class="">Welcome to Brilliance</h2>
                <p>Welcome to Brillance - Where Every Stone Holds a Story</p>
-               <a href="login.php" class="btn-get-started">Login</a>
+               <a href="client/action/login.php" class="btn-get-started">Login/Register</a>
             </div>
          </div>
       </div>
